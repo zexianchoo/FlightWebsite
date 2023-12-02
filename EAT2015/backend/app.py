4775f8b5a6448ef70c9f8bbc766df17f76f0beaa
Ze Xian Choo <99162756+zexianchoo@users.noreply.github.com>
2023-12-02 14:36:23 -0600

    backend kinda set up

---
+++ EAT2015/backend/app.py
@@ -0,0 +1,79 @@
+import os
+from google.cloud.sql.connector import Connector, IPTypes
+import pymysql
+import sqlalchemy
+import simplejson as json
+from dotenv import load_dotenv
+
+from flask import Flask
+from flask_cors import CORS
+from flask import request, session
+from flask_session import Session
+
+app = Flask(__name__)
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+app.secret_key = os.getenv("APP_SECRET_KEY")
+app.config["SESSION_COOKIE_SAMESITE"] = "None"
+app.config["SESSION_COOKIE_SECURE"] = True
+Session(app)
+CORS(app, supports_credentials=True)
+load_dotenv()
+
+def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+    """
+    Initializes a connection pool for a Cloud SQL instance of MySQL.
+
+    Uses the Cloud SQL Python Connector package.
+    """
+    # Note: Saving credentials in environment variables is convenient, but not
+    # secure - consider a more secure solution such as
+    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    # keep secrets safe.
+
+    instance_connection_name = os.environ[
+        "INSTANCE_CONNECTION_NAME"
+    ]  # e.g. 'project:region:instance'
+    db_user = os.environ["DB_USER"]  # e.g. 'my-db-user'
+    db_pass = os.environ["DB_PASS"]  # e.g. 'my-db-password'
+    db_name = os.environ["DB_NAME"]  # e.g. 'my-database'
+
+    ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
+
+    connector = Connector(ip_type)
+
+    def getconn() -> pymysql.connections.Connection:
+        conn: pymysql.connections.Connection = connector.connect(
+            instance_connection_name,
+            "pymysql",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+        )
+        return conn
+
+    pool = sqlalchemy.create_engine(
+        "mysql+pymysql://",
+        creator=getconn,
+        # ...
+    )
+    return pool
+
+
+print("SUCCESS")
+@app.route('/get_routes', methods=['GET'])
+def get_routes():
+    pool = connect_with_connector()
+    ans = []
+    with pool.connect() as db_conn:
+        rows = db_conn.execute(
+            statement=sqlalchemy.text("SELECT RouteLongName FROM Routes WHERE RouteLongName LIKE :route_name"), 
+            parameters=dict(route_name=f"%{request.args.get('search_query')}%")
+        ).fetchall()
+        for row in rows:
+            ans.append(tuple(row))
+    pool.dispose()
+    return json.dumps(ans)
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
