7dd885fecaf8c14cef6f16cd17af0a474e5b8af3
PhilipPaulson <idcbouturaddz@gmail.com>
2023-12-02 16:09:53 -0600

    weekday functionality added

--- EAT2015/frontend/src/pages/Home.jsx
+++ EAT2015/frontend/src/pages/Home.jsx
@@ -3,17 +3,19 @@ import React, { useState } from 'react';
 export const Home = () => {
   const [startDate, setStartDate] = useState('2015-01-01');
   const [endDate, setEndDate] = useState('2015-12-31');
+  const [dayOfWeek, setDayOfWeek] = useState('NA');
 
   const handleSearch = () => {
     console.log('Start Date:', startDate);
     console.log('End Date:', endDate);
-    // Here you would typically make a request to the backend with the startDate and endDate
+    console.log('Day of Week:', dayOfWeek);
+    // make a request to the backend with the startDate, endDate, and dayOfWeek after API is written
   };
 
   return (
     <main>
-      <h1>Search for Data in 2015</h1>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <h1>Search for Flight Data in 2015</h1>
+      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
         <div>
           <label htmlFor="start-date">Start Date:</label>
           <input 
@@ -23,7 +25,7 @@ export const Home = () => {
             onChange={(e) => setStartDate(e.target.value)}
             min="2015-01-01"
             max="2015-12-31"
-            style={{ marginRight: '10px' }} // Reduced space between the fields
+            style={{ marginRight: '10px' }} 
           />
         </div>
         <div>
@@ -38,7 +40,31 @@ export const Home = () => {
           />
         </div>
       </div>
-      <button onClick={handleSearch}>Search</button>
+      <div style={{ textAlign: 'center', margin: '10px' }}>
+        OR
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
+        <div>
+          <label htmlFor="day-of-week">Day of Week:</label>
+          <select 
+            id="day-of-week" 
+            value={dayOfWeek} 
+            onChange={(e) => setDayOfWeek(e.target.value)}
+          >
+            <option value="NA">N/A</option>
+            <option value="Sunday">Sunday</option>
+            <option value="Monday">Monday</option>
+            <option value="Tuesday">Tuesday</option>
+            <option value="Wednesday">Wednesday</option>
+            <option value="Thursday">Thursday</option>
+            <option value="Friday">Friday</option>
+            <option value="Saturday">Saturday</option>
+          </select>
+        </div>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <button onClick={handleSearch}>Search</button>
+      </div>
     </main>
   );
 }
