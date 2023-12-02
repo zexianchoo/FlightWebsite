f41a775ca084a4c59da44394d1464cf92913ec24
PhilipPaulson <idcbouturaddz@gmail.com>
2023-12-02 15:40:08 -0600

    Implemented Hansen's changes

--- EAT2015/frontend/src/pages/Home.jsx
+++ EAT2015/frontend/src/pages/Home.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const Home = () => {
+  const [startDate, setStartDate] = useState('2015-01-01');
+  const [endDate, setEndDate] = useState('2015-12-31');
+
+  const handleSearch = () => {
+    console.log('Start Date:', startDate);
+    console.log('End Date:', endDate);
+    // Here you would typically make a request to the backend with the startDate and endDate
+  };
+
   return (
-    <main className=''>
-        <div className='flex-col gap-y-10 justify-between'>
-            <div className='text-4xl my-20'>
-                Home
-            </div>
-            <div className='text-4xl '>
-                Text
-            </div>
-
+    <main>
+      <h1>Search for Data in 2015</h1>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div>
+          <label htmlFor="start-date">Start Date:</label>
+          <input 
+            id="start-date"
+            type="date" 
+            value={startDate} 
+            onChange={(e) => setStartDate(e.target.value)}
+            min="2015-01-01"
+            max="2015-12-31"
+            style={{ marginRight: '10px' }} // Reduced space between the fields
+          />
         </div>
-
+        <div>
+          <label htmlFor="end-date">End Date:</label>
+          <input 
+            id="end-date"
+            type="date" 
+            value={endDate} 
+            onChange={(e) => setEndDate(e.target.value)}
+            min="2015-01-01"
+            max="2015-12-31"
+          />
+        </div>
+      </div>
+      <button onClick={handleSearch}>Search</button>
     </main>
-
-  )
+  );
 }
+
+export default Home;
