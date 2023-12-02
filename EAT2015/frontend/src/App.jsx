99ed72ca3aa64c3f735cea00b602c5ffd1e77086
Ze Xian Choo <99162756+zexianchoo@users.noreply.github.com>
2023-12-02 14:02:55 -0600

    added login and home pages

--- EAT2015/frontend/src/App.jsx
+++ EAT2015/frontend/src/App.jsx
@@ -1,24 +1,26 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Login } from './pages/Login'
+import { Home } from './pages/Home'
 import './App.css'
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="App">
+    <div className="App">
       <Router>
         <Routes>
+          <Route path = "/" element={<Home /> } />
           <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
-    </>
   )
 }
 
---
+++ EAT2015/frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <main className=''>
+        <div className='flex-col gap-y-10 justify-between'>
+            <div className='text-4xl my-20'>
+                Home
+            </div>
+            <div className='text-4xl '>
+                Text
+            </div>
+
+        </div>
+
+    </main>
+
+  )
+}
